abd1e420105036101c4756713fc5810bbaed955f
Leo <nascimentoleo899@gmail.com>
2023-09-02 22:23:09 -0300

    configure teste in memory

---
+++ src/test/resources/sql/usuarios/usuarion-insert.sql
@@ -0,0 +1,3 @@
+insert into USUARIOS (id, username, password, role) value(100, 'email01@email.com', '123456', 'ROLE_ADMIN');
+insert into USUARIOS (id, username, password, role) value(101, 'email02@email.com', '654321', 'ROLE_CLIENTE');
+insert into USUARIOS (id, username, password, role) value(103, 'email03@email.com', '147258', 'ROLE_CLIENTE');
